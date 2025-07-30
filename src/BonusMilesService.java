2d408bf7b560c5a91c79fa1c2f1548d9c9c59dd6
YuriyVolodin <YourAenkinel@gmail.com>
2025-07-30 21:03:19 +0500

    Update BonusMilesService.java
    Поменял большие буквы на маленькие

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,8 +1,8 @@
 public class BonusMilesService {
-    public int calculate(int Price) {
+    public int calculate(int price) {
         int bonus;
         int rub = 20;
-        bonus = (Price / rub);
+        bonus = (price / rub);
         return bonus;
     }
 }
